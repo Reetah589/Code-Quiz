f0972654a502121e61f46029bcc267d3c86dce05
reetah589 <reetah589@gmail.com>
2021-04-18 18:52:56 -0500

    functions added

--- quiz.js
+++ quiz.js
@@ -1,4 +1,50 @@
-// Coding Quiz Challenge: Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!
+const quizContainer = document.getElementById('quiz');
+const scoresContainer = document.getElementById('scores');
+const submitButton = document.getElementById('submit');
+
+function createQuiz(){};
+function quizScores(){};
+
+function buildQuiz();
+
+    const myQuestions = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        answers: {
+            a: "Strings",
+            b: "Booleans",
+            c: "Alerts",
+            d: "Numbers"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        answers: {
+            a: "Quotes",
+            b: "Curly Brackets",
+            c: "Parenthesis",
+            d: "Square Brackets",
+        },
+        correctAnswer: "c"
+    },
+];
+
+
+
+
+// Arrays in JavaScript can be used to store _____.  A. Numbers and Strings  B. Other Arrays  C. Booleans  D. All Of The Above
+
+// String values must be enclosed within _____ when being assigned to variables.  A. Commas  B. Curly Brackets  C. Quotes  D. Parenthesis
+
+// A very useful too used during development and debugging for printing content to the debugger is: A. JavaScript  B. Terminal/Bash  C. For Loops  D. Console.log
+
+// ALL done! Your final score is ## Enter initials: 
+
+// High scores 
+
+
+
 
 // Commonly used data types DO Not Include: A. Strings  B. Booleans  C. Alerts  D. Numbers
 
